7ded77899099f8751cfa5522e1a72a8ecd1e57a7
Thiago G. Alves <thiagogarciaalves@mail.com>
2018-04-10 08:15:14 -0300

    Removendo imports...

--- src/main/java/br/fatecsp/engsw3/battleship/nave/NaveRepository.java
+++ src/main/java/br/fatecsp/engsw3/battleship/nave/NaveRepository.java
@@ -1,6 +1,5 @@
 package br.fatecsp.engsw3.battleship.nave;
 
-import br.fatecsp.engsw3.battleship.model.Nave;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NaveRepository extends CrudRepository<Nave, Integer> {
