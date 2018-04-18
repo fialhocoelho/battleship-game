accd07fc64f1e258aec209bfbf7729e4c7c52f36
Thiago G. Alves <thiagogarciaalves@mail.com>
2018-04-18 08:31:45 -0300

    Ajustando o fetch das roles

--- src/main/java/br/fatecsp/engsw3/battleship/security/user/User.java
+++ src/main/java/br/fatecsp/engsw3/battleship/security/user/User.java
@@ -7,10 +7,7 @@ import org.hibernate.annotations.CascadeType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.List;
@@ -29,7 +26,7 @@ public class User implements UserDetails {
 	@NotNull
 	private String password;
 
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
     @Cascade(CascadeType.ALL)
 	private List<Role> roles;
 
