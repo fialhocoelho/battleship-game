2dfc03cf168415d4218d74f921cdf34f304bda55
Thiago G. Alves <thiagogarciaalves@mail.com>
2018-04-18 06:16:49 -0300

    Arrumando algumas configurações.

--- src/main/java/br/fatecsp/engsw3/battleship/match/Match.java
+++ src/main/java/br/fatecsp/engsw3/battleship/match/Match.java
@@ -5,23 +5,31 @@ import br.fatecsp.engsw3.battleship.ship.Ship;
 import br.fatecsp.engsw3.battleship.user.User;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Entity
 @Data
+@Entity
+@Table(name = "matches") // MySQL não permite table com o nome match
 public class Match {
 
     @Id
     @GeneratedValue
     private int id;
+
+    @ManyToOne
     private Scenery scenery;
+
+    @ManyToOne
     private User user1;
+
+    @ManyToOne
     private User user2;
+
+    @OneToMany
     private List<Ship> ships;
+
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private boolean[][] shots1;
--- src/main/java/br/fatecsp/engsw3/battleship/ship/Ship.java
+++ src/main/java/br/fatecsp/engsw3/battleship/ship/Ship.java
@@ -4,9 +4,7 @@ import br.fatecsp.engsw3.battleship.shipmodel.ShipModel;
 import br.fatecsp.engsw3.battleship.user.User;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -15,8 +13,13 @@ public class Ship {
     @Id
     @GeneratedValue
     private int id;
+
+    @ManyToOne
     private ShipModel model;
+
+    @ManyToOne
     private User user;
+
     private int startPositionLine;
     private int startPositionColumn;
     private int endPositionLine;
